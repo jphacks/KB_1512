99051698bab526d2d81ae1babe59f8b6818b5940
Tomoya Tabuchi <tomoyat1@gmail.com>
2015-11-28 15:44:15 +0900

    High output on IO 6 for number of seconds specified by argument

--- unit/pump.c
+++ unit/pump.c
@@ -11,37 +11,58 @@
 
 int main(int argc, char **argv)
 {
-	char *board_name = mraa_get_platform_name();
+	int dur;
 	int i2c_bus;
 	int i2c_adapter;
 
 	int state = 0;
 
-	fprintf(stdout, "hello mraa\n Version: %s\n Running on %s\n", mraa_get_version(), board_name);
+	if (argc != 2) {
+		fprintf(stdout, "usage: pump duration\n");
+		return EXIT_FAILURE;
+	}
+
+	dur = atoi(argv[1]);
+
+	if (dur > 30) {
+		fprintf(stdout, "Duration too long\n");
+		return EXIT_FAILURE;
+	} else if (dur <= 0) {
+		fprintf(stdout, "Duration must be positive\n");
+		return EXIT_FAILURE;
+	}
+
+
+	mraa_init();
+	fprintf(stdout, "Output HIGH for %d seconds\n", dur);
+	fflush(stdout);
 
 	mraa_gpio_context gpio;
 	gpio = mraa_gpio_init(6);
 	mraa_gpio_dir(gpio, MRAA_GPIO_OUT);
-	fprintf(stdout, "gpio init");
+	mraa_gpio_write(gpio, 0);
 
-	while(1) {
-		/*
-		if (state == OUTPUT_ON) {
-			fprintf(stdout, "state: 0->1");
+	mraa_gpio_write(gpio, 1);
+	sleep(atoi(argv[1]));
+	mraa_gpio_write(gpio, 0);
+	
+	/*while(1) {
+		if (state != OUTPUT_ON) {
+			fprintf(stdout, "output 0->1\n");
 			mraa_gpio_write(gpio, 1);
-			state = 0;
-		} else {
-			fprintf(stdout, "state: 1->0");
-			mraa_gpio_write(gpio, 0);
 			state = 1;
+		} else {
+			fprintf(stdout, "output 1->0\n");
+			mraa_gpio_write(gpio, 0);
+			state = 0;
 		}
-		*/
-		fprintf(stdout, "state: 1->0");
-		//mraa_gpio_write(gpio, 1);
+		mraa_gpio_write(gpio, 1);
 		sleep(5);
 	}
+	*/
+	
 
-	//mraa_gpio_close(gpio);
+	mraa_gpio_close(gpio);
 
 	mraa_deinit();
 	return MRAA_SUCCESS;
