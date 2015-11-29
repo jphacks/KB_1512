b2a119f8d4685e96a2bf338ab1a933b9bba13b27
Tomoya Tabuchi <tomoyat1@gmail.com>
2015-11-29 12:00:35 +0900

    Send temperature, humidity to server

--- unit/poller/index.js
+++ unit/poller/index.js
@@ -4,22 +4,31 @@ var SensorTag = require('sensortag');
 var client = mqtt.connect('mqtt://tree:tree@m11.cloudmqtt.com:19539');
 
 client.on('connect', function () {
-	client.publish('sensor', "foo");
 	SensorTag.discoverAll(function (sensorTag) {
 		console.log('Sensor found: ' + sensorTag);
 		sensorTag.connectAndSetUp(function (error) {
 			if (error)
 				console.log("setup:" + error);
 			setTimeout(function() {
-				sensorTag.enableIrTemperature(function (error) {
+				sensorTag.enableHumidity(function (error) {
 					if (error)
 						console.log("enableIrTemp" + error);	
 					setInterval(function() {
+						var data = {
+							'uuid': 0,
+							'temperature': 0,
+							'humidity': 0
+						};
 						console.log('polling');
-						sensorTag.readIrTemperature(function (error, objectTemperature, ambientTemperature) {
-							console.log(ambientTemperature);
+						sensorTag.readHumidity(function (error, temperature, humidity) {
+							console.log("temp: " + temperature);
+							data['temperature'] = temperature;
+							console.log("humidity :" + humidity);
+							data['humidity'] = humidity;
+							client.publish('sensor', JSON.stringify(data));
+							console.log(JSON.stringify(data));
 						});
-					}, 1000);
+					}, 6000);
 				});
 			}, 3000);
 		});
